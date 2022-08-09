e24915ec6002985f2357f74faa9cda761550fc7f
xuzhouy <xuzhouy@isoftstone.com>
2022-08-09 03:04:00 +0000

    calendar code commit 08091103
    Signed-off-by: xuzhouy <xuzhouy@isoftstone.com>

---
+++ yangfan/samples/calendar/entry/src/main/ets/mainability/config/LayoutConstant.ts
@@ -0,0 +1,30 @@
+/*
+# Copyright (c) 2020-2030 iSoftStone Information Technology (Group) Co.,Ltd.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+*/
+
+export class LayoutConstant {
+    WH_5_100 = '5%'
+    WH_25_100 = '25%'
+    WH_30_100 = '30%'
+    WH_50_100 = '50%'
+    WH_70_100 = '70%'
+    WH_75_100 = '75%'
+    WH_80_100 = '80%'
+    WH_90_100 = '90%'
+    WH_95_100 = '95%'
+    WH_100_100 = '100%'
+}
+
+let layoutConstant = new LayoutConstant()
+
+export default layoutConstant as LayoutConstant
