ec603bdb6bff9d953fc0f558b65d1f609c99cd4f
xuzhouy <xuzhouy@isoftstone.com>
2022-08-11 07:06:04 +0000

    inputcore code commit 08111505
    Signed-off-by: xuzhouy <xuzhouy@isoftstone.com>

---
+++ yangfan/samples/inputcore/entry/.preview/intermediates/res/debug/rich/assets/js/mainability/pages/second/second_.js
@@ -0,0 +1,385 @@
+/*
+# Copyright (c) 2020-2030 iSoftStone Information Technology (Group) Co.,Ltd.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+*/
+
+(global.___mainEntry___ = function (globalObjects) {
+  var define = globalObjects.define;
+  var require = globalObjects.require;
+  var bootstrap = globalObjects.bootstrap;
+  var register = globalObjects.register;
+  var render = globalObjects.render;
+  var $app_define$ = globalObjects.$app_define$;
+  var $app_bootstrap$ = globalObjects.$app_bootstrap$;
+  var $app_require$ = globalObjects.$app_require$;
+  var history = globalObjects.history;
+  var Image = globalObjects.Image;
+  var OffscreenCanvas = globalObjects.OffscreenCanvas;
+  (function(global) {
+    "use strict";
+var _bf5a3b5d58ba2355a6c4a7edd3c87580;
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "E:\\05.openHarmonyHaps\\inputcore\\entry\\src\\main\\js\\MainAbility\\pages\\second\\second.hml?entry":
+/*!***************************************************************************************************!*\
+  !*** E:\05.openHarmonyHaps\inputcore\entry\src\main\js\MainAbility\pages\second\second.hml?entry ***!
+  \***************************************************************************************************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+var $app_template$ = __webpack_require__(/*! !!./lib/json.js!./lib/template.js!./second.hml */ "./lib/json.js!./lib/template.js!E:\\05.openHarmonyHaps\\inputcore\\entry\\src\\main\\js\\MainAbility\\pages\\second\\second.hml")
+var $app_style$ = __webpack_require__(/*! !!./lib/json.js!./lib/style.js!./second.css */ "./lib/json.js!./lib/style.js!E:\\05.openHarmonyHaps\\inputcore\\entry\\src\\main\\js\\MainAbility\\pages\\second\\second.css")
+var $app_script$ = __webpack_require__(/*! !!./lib/script.js!./node_modules/babel-loader?presets[]=C:\Users\weiliuq\AppData\Local\OpenHarmony\Sdk\js\3.1.0.0\build-tools\ace-loader\node_modules\@babel\preset-env&plugins[]=C:\Users\weiliuq\AppData\Local\OpenHarmony\Sdk\js\3.1.0.0\build-tools\ace-loader\node_modules\@babel\plugin-transform-modules-commonjs&comments=false!./lib/resource-reference-script.js!./second.js */ "./lib/script.js!./node_modules/babel-loader/lib/index.js?presets[]=C:\\Users\\weiliuq\\AppData\\Local\\OpenHarmony\\Sdk\\js\\3.1.0.0\\build-tools\\ace-loader\\node_modules\\@babel\\preset-env&plugins[]=C:\\Users\\weiliuq\\AppData\\Local\\OpenHarmony\\Sdk\\js\\3.1.0.0\\build-tools\\ace-loader\\node_modules\\@babel\\plugin-transform-modules-commonjs&comments=false!./lib/resource-reference-script.js!E:\\05.openHarmonyHaps\\inputcore\\entry\\src\\main\\js\\MainAbility\\pages\\second\\second.js")
+
+$app_define$('@app-component/second', [], function($app_require$, $app_exports$, $app_module$) {
+
+$app_script$($app_module$, $app_exports$, $app_require$)
+if ($app_exports$.__esModule && $app_exports$.default) {
+$app_module$.exports = $app_exports$.default
+}
+
+$app_module$.exports.template = $app_template$
+
+$app_module$.exports.style = $app_style$
+
+})
+$app_bootstrap$('@app-component/second',undefined,undefined)
+
+/***/ }),
+
+/***/ "./lib/json.js!./lib/style.js!E:\\05.openHarmonyHaps\\inputcore\\entry\\src\\main\\js\\MainAbility\\pages\\second\\second.css":
+/*!**************************************************************************************************************************!*\
+  !*** ./lib/json.js!./lib/style.js!E:\05.openHarmonyHaps\inputcore\entry\src\main\js\MainAbility\pages\second\second.css ***!
+  \**************************************************************************************************************************/
+/***/ ((module) => {
+
+module.exports = {
+  ".container": {
+    "display": "flex",
+    "flexDirection": "column",
+    "justifyContent": "center",
+    "alignItems": "center",
+    "left": "0px",
+    "top": "0px",
+    "width": "100%",
+    "height": "100%"
+  },
+  ".title": {
+    "fontSize": "60px",
+    "textAlign": "center",
+    "width": "100%",
+    "height": "40%",
+    "marginTop": "10px",
+    "marginRight": "10px",
+    "marginBottom": "10px",
+    "marginLeft": "10px"
+  },
+  ".btn": {
+    "width": "50%",
+    "height": "100px",
+    "fontSize": "40px"
+  }
+}
+
+/***/ }),
+
+/***/ "./lib/json.js!./lib/template.js!E:\\05.openHarmonyHaps\\inputcore\\entry\\src\\main\\js\\MainAbility\\pages\\second\\second.hml":
+/*!*****************************************************************************************************************************!*\
+  !*** ./lib/json.js!./lib/template.js!E:\05.openHarmonyHaps\inputcore\entry\src\main\js\MainAbility\pages\second\second.hml ***!
+  \*****************************************************************************************************************************/
+/***/ ((module) => {
+
+module.exports = {
+  "attr": {
+    "debugLine": "pages/second/second:1",
+    "className": "container"
+  },
+  "type": "div",
+  "classList": [
+    "container"
+  ],
+  "children": [
+    {
+      "attr": {
+        "debugLine": "pages/second/second:2",
+        "className": "title",
+        "value": function () {return this.$t('strings.page')}
+      },
+      "type": "text",
+      "classList": [
+        "title"
+      ]
+    },
+    {
+      "attr": {
+        "debugLine": "pages/second/second:5",
+        "className": "btn",
+        "type": "button",
+        "value": function () {return this.$t('strings.back')}
+      },
+      "type": "input",
+      "classList": [
+        "btn"
+      ],
+      "onBubbleEvents": {
+        "click": "onclick"
+      }
+    }
+  ]
+}
+
+/***/ }),
+
+/***/ "./lib/script.js!./node_modules/babel-loader/lib/index.js?presets[]=C:\\Users\\weiliuq\\AppData\\Local\\OpenHarmony\\Sdk\\js\\3.1.0.0\\build-tools\\ace-loader\\node_modules\\@babel\\preset-env&plugins[]=C:\\Users\\weiliuq\\AppData\\Local\\OpenHarmony\\Sdk\\js\\3.1.0.0\\build-tools\\ace-loader\\node_modules\\@babel\\plugin-transform-modules-commonjs&comments=false!./lib/resource-reference-script.js!E:\\05.openHarmonyHaps\\inputcore\\entry\\src\\main\\js\\MainAbility\\pages\\second\\second.js":
+/*!**********************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./lib/script.js!./node_modules/babel-loader/lib/index.js?presets[]=C:\Users\weiliuq\AppData\Local\OpenHarmony\Sdk\js\3.1.0.0\build-tools\ace-loader\node_modules\@babel\preset-env&plugins[]=C:\Users\weiliuq\AppData\Local\OpenHarmony\Sdk\js\3.1.0.0\build-tools\ace-loader\node_modules\@babel\plugin-transform-modules-commonjs&comments=false!./lib/resource-reference-script.js!E:\05.openHarmonyHaps\inputcore\entry\src\main\js\MainAbility\pages\second\second.js ***!
+  \**********************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = function(module, exports, $app_require$){"use strict";
+
+var _interopRequireDefault = __webpack_require__(/*! @babel/runtime/helpers/interopRequireDefault */ "./node_modules/@babel/runtime/helpers/interopRequireDefault.js");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _system = _interopRequireDefault(requireModule("@system.router"));
+
+var _default = {
+  data: {
+    title: 'World'
+  },
+  onclick: function onclick() {
+    _system["default"].replace({
+      uri: "pages/index/index"
+    });
+  }
+};
+exports["default"] = _default;
+
+function requireModule(moduleName) {
+  const systemList = ['system.router', 'system.app', 'system.prompt', 'system.configuration',
+  'system.image', 'system.device', 'system.mediaquery', 'ohos.animator', 'system.grid', 'system.resource']
+  var target = ''
+  if (systemList.includes(moduleName.replace('@', ''))) {
+    target = $app_require$('@app-module/' + moduleName.substring(1));
+    return target;
+  }
+  var shortName = moduleName.replace(/@[^.]+.([^.]+)/, '$1');
+  target = requireNapi(shortName);
+  if (typeof target !== 'undefined' && /@ohos/.test(moduleName)) {
+    return target;
+  }
+  if (typeof ohosplugin !== 'undefined' && /@ohos/.test(moduleName)) {
+    target = ohosplugin;
+    for (let key of shortName.split('.')) {
+      target = target[key];
+      if(!target) {
+        break;
+      }
+    }
+    if (typeof target !== 'undefined') {
+      return target;
+    }
+  }
+  if (typeof systemplugin !== 'undefined') {
+    target = systemplugin;
+    for (let key of shortName.split('.')) {
+      target = target[key];
+      if(!target) {
+        break;
+      }
+    }
+    if (typeof target !== 'undefined') {
+      return target;
+    }
+  }
+  return target;
+}
+
+var moduleOwn = exports.default || module.exports;
+var accessors = ['public', 'protected', 'private'];
+if (moduleOwn.data && accessors.some(function (acc) {
+    return moduleOwn[acc];
+  })) {
+  throw new Error('For VM objects, attribute data must not coexist with public, protected, or private. Please replace data with public.');
+} else if (!moduleOwn.data) {
+  moduleOwn.data = {};
+  moduleOwn._descriptor = {};
+  accessors.forEach(function(acc) {
+    var accType = typeof moduleOwn[acc];
+    if (accType === 'object') {
+      moduleOwn.data = Object.assign(moduleOwn.data, moduleOwn[acc]);
+      for (var name in moduleOwn[acc]) {
+        moduleOwn._descriptor[name] = {access : acc};
+      }
+    } else if (accType === 'function') {
+      console.warn('For VM objects, attribute ' + acc + ' value must not be a function. Change the value to an object.');
+    }
+  });
+}}
+/* generated by ace-loader */
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		var commonCachedModule = globalThis["__common_module_cache___bf5a3b5d58ba2355a6c4a7edd3c87580"] ? globalThis["__common_module_cache___bf5a3b5d58ba2355a6c4a7edd3c87580"][moduleId]: null;
+/******/ 		if (commonCachedModule) { return commonCachedModule.exports; }
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		function isCommonModue(moduleId) {
+/******/ 		              if (globalThis["webpackChunk_bf5a3b5d58ba2355a6c4a7edd3c87580"]) {
+/******/ 		                const length = globalThis["webpackChunk_bf5a3b5d58ba2355a6c4a7edd3c87580"].length;
+/******/ 		                switch (length) {
+/******/ 		                  case 1:
+/******/ 		                    return globalThis["webpackChunk_bf5a3b5d58ba2355a6c4a7edd3c87580"][0][1][moduleId];
+/******/ 		                  case 2:
+/******/ 		                    return globalThis["webpackChunk_bf5a3b5d58ba2355a6c4a7edd3c87580"][0][1][moduleId] ||
+/******/ 		                    globalThis["webpackChunk_bf5a3b5d58ba2355a6c4a7edd3c87580"][1][1][moduleId];
+/******/ 		                }
+/******/ 		              }
+/******/ 		              return undefined;
+/******/ 		            }
+/******/ 		if (globalThis["__common_module_cache___bf5a3b5d58ba2355a6c4a7edd3c87580"] && moduleId.indexOf("?name=") < 0 && isCommonModue(moduleId)) {
+/******/ 		  globalThis["__common_module_cache___bf5a3b5d58ba2355a6c4a7edd3c87580"][moduleId] = module;
+/******/ 		}
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/chunk loaded */
+/******/ 	(() => {
+/******/ 		var deferred = [];
+/******/ 		__webpack_require__.O = (result, chunkIds, fn, priority) => {
+/******/ 			if(chunkIds) {
+/******/ 				priority = priority || 0;
+/******/ 				for(var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) deferred[i] = deferred[i - 1];
+/******/ 				deferred[i] = [chunkIds, fn, priority];
+/******/ 				return;
+/******/ 			}
+/******/ 			var notFulfilled = Infinity;
+/******/ 			for (var i = 0; i < deferred.length; i++) {
+/******/ 				var [chunkIds, fn, priority] = deferred[i];
+/******/ 				var fulfilled = true;
+/******/ 				for (var j = 0; j < chunkIds.length; j++) {
+/******/ 					if ((priority & 1 === 0 || notFulfilled >= priority) && Object.keys(__webpack_require__.O).every((key) => (__webpack_require__.O[key](chunkIds[j])))) {
+/******/ 						chunkIds.splice(j--, 1);
+/******/ 					} else {
+/******/ 						fulfilled = false;
+/******/ 						if(priority < notFulfilled) notFulfilled = priority;
+/******/ 					}
+/******/ 				}
+/******/ 				if(fulfilled) {
+/******/ 					deferred.splice(i--, 1)
+/******/ 					var r = fn();
+/******/ 					if (r !== undefined) result = r;
+/******/ 				}
+/******/ 			}
+/******/ 			return result;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	(() => {
+/******/ 		// no baseURI
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			"./pages/second/second": 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		__webpack_require__.O.j = (chunkId) => (installedChunks[chunkId] === 0);
+/******/ 		
+/******/ 		// install a JSONP callback for chunk loading
+/******/ 		var webpackJsonpCallback = (parentChunkLoadingFunction, data) => {
+/******/ 			var [chunkIds, moreModules, runtime] = data;
+/******/ 			// add "moreModules" to the modules object,
+/******/ 			// then flag all "chunkIds" as loaded and fire callback
+/******/ 			var moduleId, chunkId, i = 0;
+/******/ 			for(moduleId in moreModules) {
+/******/ 				if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 					__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 				}
+/******/ 			}
+/******/ 			if(runtime) var result = runtime(__webpack_require__);
+/******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
+/******/ 			for(;i < chunkIds.length; i++) {
+/******/ 				chunkId = chunkIds[i];
+/******/ 				if(__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 					installedChunks[chunkId][0]();
+/******/ 				}
+/******/ 				installedChunks[chunkIds[i]] = 0;
+/******/ 			}
+/******/ 			return __webpack_require__.O(result);
+/******/ 		}
+/******/ 		
+/******/ 		var chunkLoadingGlobal = globalThis["webpackChunk_bf5a3b5d58ba2355a6c4a7edd3c87580"] = globalThis["webpackChunk_bf5a3b5d58ba2355a6c4a7edd3c87580"] || [];
+/******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
+/******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["vendors"], () => (__webpack_require__("E:\\05.openHarmonyHaps\\inputcore\\entry\\src\\main\\js\\MainAbility\\pages\\second\\second.hml?entry")))
+/******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
+/******/ 	_bf5a3b5d58ba2355a6c4a7edd3c87580 = __webpack_exports__;
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=second.js.map
+})(this.__appProto__);
+})
---
+++ yangfan/samples/inputcore/entry/.preview/intermediates/res/debug/rich/assets/js/mainability/vendors_.js
@@ -0,0 +1,138 @@
+/*
+# Copyright (c) 2020-2030 iSoftStone Information Technology (Group) Co.,Ltd.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+*/
+
+"use strict";
+(globalThis["webpackChunk_bf5a3b5d58ba2355a6c4a7edd3c87580"] = globalThis["webpackChunk_bf5a3b5d58ba2355a6c4a7edd3c87580"] || []).push([["vendors"],{
+
+/***/ "./node_modules/@babel/runtime/helpers/interopRequireDefault.js":
+/*!**********************************************************************!*\
+  !*** ./node_modules/@babel/runtime/helpers/interopRequireDefault.js ***!
+  \**********************************************************************/
+/***/ ((module) => {
+
+
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : {
+    "default": obj
+  };
+}
+
+module.exports = _interopRequireDefault, module.exports.__esModule = true, module.exports.default = module.exports;
+
+function requireModule(moduleName) {
+  const systemList = ['system.router', 'system.app', 'system.prompt', 'system.configuration',
+  'system.image', 'system.device', 'system.mediaquery', 'ohos.animator', 'system.grid', 'system.resource']
+  var target = ''
+  if (systemList.includes(moduleName.replace('@', ''))) {
+    target = $app_require$('@app-module/' + moduleName.substring(1));
+    return target;
+  }
+  var shortName = moduleName.replace(/@[^.]+.([^.]+)/, '$1');
+  target = requireNapi(shortName);
+  if (typeof target !== 'undefined' && /@ohos/.test(moduleName)) {
+    return target;
+  }
+  if (typeof ohosplugin !== 'undefined' && /@ohos/.test(moduleName)) {
+    target = ohosplugin;
+    for (let key of shortName.split('.')) {
+      target = target[key];
+      if(!target) {
+        break;
+      }
+    }
+    if (typeof target !== 'undefined') {
+      return target;
+    }
+  }
+  if (typeof systemplugin !== 'undefined') {
+    target = systemplugin;
+    for (let key of shortName.split('.')) {
+      target = target[key];
+      if(!target) {
+        break;
+      }
+    }
+    if (typeof target !== 'undefined') {
+      return target;
+    }
+  }
+  return target;
+}
+
+
+/***/ }),
+
+/***/ "./node_modules/@babel/runtime/helpers/newArrowCheck.js":
+/*!**************************************************************!*\
+  !*** ./node_modules/@babel/runtime/helpers/newArrowCheck.js ***!
+  \**************************************************************/
+/***/ ((module) => {
+
+
+
+function _newArrowCheck(innerThis, boundThis) {
+  if (innerThis !== boundThis) {
+    throw new TypeError("Cannot instantiate an arrow function");
+  }
+}
+
+module.exports = _newArrowCheck, module.exports.__esModule = true, module.exports.default = module.exports;
+
+function requireModule(moduleName) {
+  const systemList = ['system.router', 'system.app', 'system.prompt', 'system.configuration',
+  'system.image', 'system.device', 'system.mediaquery', 'ohos.animator', 'system.grid', 'system.resource']
+  var target = ''
+  if (systemList.includes(moduleName.replace('@', ''))) {
+    target = $app_require$('@app-module/' + moduleName.substring(1));
+    return target;
+  }
+  var shortName = moduleName.replace(/@[^.]+.([^.]+)/, '$1');
+  target = requireNapi(shortName);
+  if (typeof target !== 'undefined' && /@ohos/.test(moduleName)) {
+    return target;
+  }
+  if (typeof ohosplugin !== 'undefined' && /@ohos/.test(moduleName)) {
+    target = ohosplugin;
+    for (let key of shortName.split('.')) {
+      target = target[key];
+      if(!target) {
+        break;
+      }
+    }
+    if (typeof target !== 'undefined') {
+      return target;
+    }
+  }
+  if (typeof systemplugin !== 'undefined') {
+    target = systemplugin;
+    for (let key of shortName.split('.')) {
+      target = target[key];
+      if(!target) {
+        break;
+      }
+    }
+    if (typeof target !== 'undefined') {
+      return target;
+    }
+  }
+  return target;
+}
+
+
+/***/ })
+
+}]);
+      globalThis["__common_module_cache___bf5a3b5d58ba2355a6c4a7edd3c87580"] = globalThis["__common_module_cache___bf5a3b5d58ba2355a6c4a7edd3c87580"] || {};
+//# sourceMappingURL=vendors.js.map
